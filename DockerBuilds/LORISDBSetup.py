2bed196d2edf623f3f421c2567d73d7ddb2d4d63
Yang Ding <dyt811@gmail.com>
2019-12-20 18:22:23 -0500

    Finishing DB incorporation and Selenium process
    - Add Selenium frontend interaction
    - Passing key configurator to selenium to fill 
    - Add Selenium driver python script. 
    - Add requiment.txt

---
+++ DockerBuilds/LORISDBSetup.py
@@ -0,0 +1,67 @@
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+import os
+#from dotenv import load_dotenv
+
+#load_dotenv()
+
+chrome = webdriver.Remote(
+          command_executor='http://localhost:4444/wd/hub',
+          desired_capabilities=DesiredCapabilities.CHROME)
+#firefox = webdriver.Remote(
+#          command_executor='http://localhost:4444/wd/hub',
+#          desired_capabilities=DesiredCapabilities.FIREFOX)
+
+port = os.getenv("Port_LORIS_HTTP")
+chrome.get(f'https://LORIS22URL:{port}/installdb.php')
+
+# Load the information to fill inside
+LorisMySQLHost = os.getenv("LorisMySQLHost")
+LorisMySQLRoot = os.getenv("LorisMySQLRoot")
+LorisMySQLRootPassword = os.getenv("LorisMySQLRootPassword")
+# Set the env based on the value of the arguments
+LorisMySQLUser = os.getenv("LorisMySQLUser")
+LorisMySQLUserPassword = os.getenv("LorisMySQLUserPassword")
+LorisFrontendUser = os.getenv("LorisFrontendUser")
+LorisFrontendPassword = os.getenv("LorisFrontendPassword")
+
+
+input_serverhost = chrome.find_element_by_id("serverhost")
+input_serverhost.send_keys(LorisMySQLHost)
+
+input_serverhost = chrome.find_element_by_id("serveruser")
+input_serverhost.send_keys(LorisMySQLRoot)
+
+input_serverhost = chrome.find_element_by_id("serverpassword")
+input_serverhost.send_keys(LorisMySQLRootPassword)
+
+button_submit = chrome.find_element_by_css_selector(".btn-submit")
+button_submit.click()
+
+# Wait 15 seconds.
+import time
+time.sleep(15)
+
+
+input_lorismysqluser = chrome.find_element_by_name("lorismysqluser")
+input_lorismysqluser.send_keys(LorisMySQLUser)
+
+input_lorismysqlpassword = chrome.find_element_by_name("lorismysqlpassword")
+input_lorismysqlpassword.send_keys(LorisMySQLUserPassword)
+
+input_frontenduser = chrome.find_element_by_name("frontenduser")
+input_frontenduser.send_keys(LorisFrontendUser)
+
+input_frontendpassword = chrome.find_element_by_name("frontendpassword")
+input_frontendpassword.send_keys(LorisFrontendPassword)
+
+button_submit = chrome.find_element_by_xpath("//input[@type='submit']")
+button_submit.click()
+chrome.quit()
+
+"""
+firefox.get('https://localhost:8080/installdb.php')
+print(firefox.title)
+firefox.quit()
+"""
+
