d3003b681ac498e3218efc1e8a07b652625858c0
Yang Ding <dyt811@gmail.com>
2020-01-13 17:00:28 -0500

    WIP: Integrated LORIS21 image with DockerHub
    - Updated dockerfiles with the proper tags to push towards DockerHub. 
    - Cleaned up LORIS-MRI to a separate DockerImage built based on base image. 
    - Updated versioning specificity
    - Added additional setup/teardown.sh to partially automated the docker-compose related tasks.

---
+++ DockerBuilds/21/setup.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# bach script to automatically remove the image/container to
+docker-compose -f ./ComposeLORIS21.0.5.yml build --pull
+docker-compose -f ./ComposeLORIS21.0.5.yml up
--- DockerBuilds/21/teardown.sh
+++ DockerBuilds/21/teardown.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 # bach script to automatically remove the image/container to
-docker-compose -f ComposeLORIS21.yml down
+docker-compose -f ComposeLORIS21.0.5.yml down
 docker image prune -f
 docker container prune -f
 
 # nuke containers
-docker rm chrome_1
-docker rm hub_1
-docker rm LORIS_1
-docker rm MySQL_1
-docker rm DC_LORIS21Configurator_v0.4
+docker rm 21_chrome_1
+docker rm 21_hub_1
+docker rm 21_loris_1
+docker rm 21_lorisdb_1
+docker rm 21_lorisdbsetup_1
 
 # nuke container images
-docker rmi 21_selenium-driver:latest
+docker rmi 21_lorisdbsetup:latest
 #docker rmi dyt811:DC_LORIS21_v0.4
 
 # sanity check on results
