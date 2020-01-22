fae9d02bb67c5cf2a109cd8ff2fcb553a261766e
Yang Ding <dyt811@gmail.com>
2020-01-22 17:17:41 -0500

    Upgrade: Added new local persistent volumes for MySQL and LORIS
    - LORIS-MRI has yet to be completely fixed.

--- DockerBuilds/21/setup.sh
+++ DockerBuilds/21/setup.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 # bach script to automatically remove the image/container to
+# Remove all pre-existing volumes.
+docker volume rm $(docker volume ls)
+
 docker-compose -f ./ComposeLORIS21.0.5.yml build
 docker-compose -f ./ComposeLORIS21.0.5.yml up
